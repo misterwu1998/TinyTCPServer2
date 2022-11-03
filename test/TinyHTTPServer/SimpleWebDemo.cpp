1231d21169a81c3ccea7fba3b5b9268915cdb504
Monte Cristo <misterwu1998@163.com>
2022-11-03 21:08:41 +0800

    小改默认日志器

--- test/TinyHTTPServer/SimpleWebDemo.cpp
+++ test/TinyHTTPServer/SimpleWebDemo.cpp
@@ -12,15 +12,14 @@
 int main(int argc, char const *argv[])
 {
   // 日志器
-  // std::shared_ptr<spdlog::logger> spdLogger = spdlog::rotating_logger_mt(
-  //     "SimpleWebDemo"
-  //   , "./temp/log/log"
-  //   , 1024*1024*4, 4);
-  // spdLogger->set_pattern("[%H:%M:%S.%e %z][%n][%l][thread %t] %v");
-  // spdLogger->set_level(spdlog::level::level_enum::trace);
-  // spdLogger->flush_on(spdlog::level::level_enum::trace);
-  // TTCPS2::Logger::initOrGet(spdLogger);
-  // 尝试缺省logger
+  std::shared_ptr<spdlog::logger> spdLogger = spdlog::rotating_logger_mt(
+      "SimpleWebDemo"
+    , "./temp/log/log"
+    , 1024*1024*4, 4);
+  spdLogger->set_pattern("[%H:%M:%S.%e %z][%n][%l][thread %t] %v");
+  spdLogger->set_level(spdlog::level::level_enum::trace);
+  spdLogger->flush_on(spdlog::level::level_enum::trace);
+  TTCPS2::Logger::initOrGet(spdLogger);
 
   std::shared_ptr<TTCPS2::HTTPHandlerFactory> HTTPSettings(
     std::make_shared<TTCPS2::HTTPHandlerFactory>()  );
@@ -145,7 +144,7 @@ int main(int argc, char const *argv[])
   });
   
   TTCPS2::TinyHTTPServer tws(
-      "127.0.0.1", 6324,32,4,HTTPSettings
+      "127.0.0.1", 6324,32,1,HTTPSettings
     , &(TTCPS2::ThreadPool::getPool(1)));
   tws.run();
   std::cout << "Input something to shutdown: ";
